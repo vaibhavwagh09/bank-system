9716272935cb73968b48f34002830429638b74a1
vaibhavwagh09 <waghvaibhav0909@gmail.com>
2021-12-27 21:58:47 +0530

    django

--- bank/settings.py
+++ bank/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +118,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles") 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
